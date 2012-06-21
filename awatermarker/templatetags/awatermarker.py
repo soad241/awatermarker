8fdf400134d19e338a073ee485c40def7498da0a
Jose Penarrieta <soad241@gmail.com>
2012-06-21 12:10:28 -0500

    generating aws auth querystrings for watermarks

--- awatermarker/templatetags/awatermarker.py
+++ awatermarker/templatetags/awatermarker.py
@@ -1,8 +1,8 @@
 from django import template
 from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
 from photo_videos.storage import get_upload_storage
 from django.utils.safestring import mark_safe
+from django.core.cache import cache
 
 register = template.Library()
 
@@ -11,15 +11,18 @@ def watermark(url):
     if not settings.SITE_ID == 1:
         return url
     image_url = url.split("?")[0]
-    params = url.split("?")[1]
     filename = "{0}.{1}".format(image_url, settings.DEFAULT_WATERMARK_SLUG)
-    st = get_upload_storage()
-    filename = filename.replace(settings.UPLOAD_MEDIA_URL, "")
-    filename = filename.replace(settings.S3_BUCKET_URL, "")
-    if st.get_storage(filename) == st.remote:
-        storage = S3BotoStorage()
-        return mark_safe(storage.url(filename))
-    return mark_safe(settings.UPLOAD_MEDIA_URL + filename)
+    if not filename.startswith(settings.S3_BUCKET_URL):
+        return filename
 
+    cache_key = "watermark-%s" % filename
+    cache_key = cache_key.strip()
+    cached_val = cache.get(cache_key)
+    if cached_val:
+        return cached_val
+    storage = get_upload_storage(delayed=True).remote
+    new_url = storage.url(filename.replace(settings.S3_BUCKET_URL, ""))
+    cache.set(cache_key, new_url, settings.WATERMARK_URL_CACHE_TIME)
+    return new_url
 
 
