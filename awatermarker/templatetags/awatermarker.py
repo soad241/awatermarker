08a4b3cbb848d43d9a8532f0824132be16906faf
Jose Penarrieta <soad241@gmail.com>
2012-06-20 20:49:07 -0500

    fix

--- awatermarker/templatetags/awatermarker.py
+++ awatermarker/templatetags/awatermarker.py
@@ -2,7 +2,7 @@ from django import template
 from django.conf import settings
 register = template.Library()
 from storages.backends.s3boto import S3BotoStorage
-
+from photo_videos.storage import get_upload_storage
 @register.simple_tag
 def watermark(url):
     if not settings.SITE_ID == 1:
@@ -10,9 +10,12 @@ def watermark(url):
     image_url = url.split("?")[0]
     params = url.split("?")[1]
     filename = "{0}.{1}".format(image_url, settings.DEFAULT_WATERMARK_SLUG)
-    filename = filename.replace(settings.S3_BUCKET_URL, "")
-    storage = S3BotoStorage()
-    return storage.url(filename)
+    st = get_upload_storage()
+    if st.get_storage(filename) == st.remote:
+        filename = filename.replace(settings.S3_BUCKET_URL, "")
+        storage = S3BotoStorage()
+        return storage.url(filename)
+    return filename
 
 
 
